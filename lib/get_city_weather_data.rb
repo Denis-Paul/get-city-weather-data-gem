a184b4e6a0e26cd7ba65ead6b4233fbe78044b68
Denis Paul <denis.paul@softvision.com>
2022-02-09 16:36:53 +0200

    intial commit

---
+++ lib/get_city_weather_data.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "get_city_weather_data/version"
+
+module GetCityWeatherData
+  class Error < StandardError; end
+
+  
+end
---
+++ lib/get_city_weather_data/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GetCityWeatherData
+  VERSION = "0.1.0"
+end
