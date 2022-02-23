f5bd684424e0efd59f87f727889fee12f64165ea
Denis Paul <denis.paul@softvision.com>
2022-02-23 18:58:18 +0200

    added setup method for API key, changes on API call logic

--- lib/get_city_weather_data.rb
+++ lib/get_city_weather_data.rb
@@ -3,6 +3,36 @@
 require_relative "get_city_weather_data/version"
 
 module GetCityWeatherData
-  autoload :Client, "get_city_weather_data/client"
+
+  # autoload :Client, "get_city_weather_data/client"
+
+  class << self
+    attr_accessor :api_key
+
+    BASE_URL = "http://api.weatherapi.com/v1/current.json"
+
+    def get_weather(city)
+        @city = city
+        get_weather_data
+    end
+
+    def setup
+        yield self
+    end
+
+    private
+
+    def get_weather_data
+        begin
+            response = RestClient.get "#{BASE_URL}?q=#{@city}", { content_type: :json, accept: :json, "key": api_key }
+            current_weather = JSON.parse(response.body, symbolize_names: true)
+            current_weather[:current]
+        rescue RestClient::ExceptionWithResponse => exception
+            api_error_message = JSON.parse(exception.response, symbolize_names: true)[:error][:message]
+            api_error_message
+        end
+    end
+  end
+
   autoload :Error, "get_city_weather_data/error"
 end
