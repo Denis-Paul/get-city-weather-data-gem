f60eabc8ea648d717df75bc9735ded3266fb8893
Denis Paul <denis.paul@softvision.com>
2022-02-23 18:56:35 +0200

    commented code in client.rb

--- lib/get_city_weather_data/client.rb
+++ lib/get_city_weather_data/client.rb
@@ -4,36 +4,38 @@ require 'dotenv'
 Dotenv.load
 # Dotenv.load(File.expand_path("../.env", __FILE__))
 
-module GetCityWeatherData
-    class Client
-        BASE_URL = "http://api.weatherapi.com/v1/current.json"
+# module GetCityWeatherData
+#     class << self
+#         BASE_URL = "http://api.weatherapi.com/v1/current.json"
 
-        attr_reader :city, :api_key
-
-        def initialize(city:, api_key: ENV["WEATHER_API_KEY"])
-            @city = city
-            @api_key = api_key
-        end
+#         attr_reader :city #, :api_key
+#         attr_accessor :api_key
     
-        def get_weather
-            get_weather_data
-        end
+#         def get_weather(city)
+#             @city = city
+#             @api_key = api_key
+#             get_weather_data
+#         end
 
-        private
+#         def config
+#             yield self
+#         end
 
-        def get_weather_data
-            begin
-                response = RestClient.get "#{BASE_URL}?q=#{@city}", { content_type: :json, accept: :json, "key": @api_key }
-                current_weather = JSON.parse(response.body, symbolize_names: true)
-                current_weather[:current]
-            rescue RestClient::ExceptionWithResponse => exception
-                api_error_message = JSON.parse(exception.response, symbolize_names: true)[:error][:message]
-                api_error_message
-            end
-        end
+#         private
 
-        # def inspect
-        #     "#<GetCityWeatherData::Client>"
-        # end
-    end
-end
+#         def get_weather_data
+#             begin
+#                 response = RestClient.get "#{BASE_URL}?q=#{@city}", { content_type: :json, accept: :json, "key": @api_key }
+#                 current_weather = JSON.parse(response.body, symbolize_names: true)
+#                 current_weather[:current]
+#             rescue RestClient::ExceptionWithResponse => exception
+#                 api_error_message = JSON.parse(exception.response, symbolize_names: true)[:error][:message]
+#                 api_error_message
+#             end
+#         end
+
+#         # def inspect
+#         #     "#<GetCityWeatherData::Client>"
+#         # end
+#     end
+# end
